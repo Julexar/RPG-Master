80d0308b84d9f7741ecb28e1bdd15cb3e6d56db4
Julexar <viktor.thorin@gmail.com>
2023-02-09 07:57:58 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    token: 'MTA0NjQyMjMyNTMyNDY4OTQyOA.GZjtSA.Q7aw_kXASkJDQpquepGqDKlu6buNdL76H91IIM',
+    default_prefix: 'r!',
+    owners: ['676518256282042393'],
+    presence: {
+        activities: [
+          {
+            name: 'TTRPGs',
+            type: 0,
+          },
+        ],
+        status: 'online',
+    },
+};
