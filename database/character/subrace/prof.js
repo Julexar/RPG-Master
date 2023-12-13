7664f0013e54e45916fb57e4b4850b81baeca734
Julexar <viktor.thorin@gmail.com>
2023-12-13 11:35:03 +0100

    Fixed Character Subrace Proficiency

--- database/character/subrace/prof.js
+++ database/character/subrace/prof.js
@@ -4,7 +4,7 @@ import { psql } from '../../psql.js';
 const query = psql.query;
 
 class CharacterSubraceProf {
-    async getAll(char, sub) {
+    static async getAll(char, sub) {
         const results = await query('SELECT * FROM character_subrace_profs WHERE char_id = $1 AND sub_id = $2', [char.id, sub.id]);
 
         if (results.length === 0) {
@@ -27,11 +27,31 @@ class CharacterSubraceProf {
         );
     }
 
-    async getOne(char, sub, prof) {
-        const results = await query('SELECT * FROM character_subrace_profs WHERE char_id = $1 AND sub_id = $2 AND id = $3', [char.id, sub.id, prof.id]);
+    static async getOne(char, sub, prof) {
+        if (prof.id) {
+            const results = await query('SELECT * FROM character_subrace_profs WHERE char_id = $1 AND sub_id = $2 AND id = $3', [char.id, sub.id, prof.id]);
+
+            if (results.length === 0) {
+                throw new NotFoundError('Character Subrace Proficiency not found', 'Could not find that Subrace Proficiency for that Character in the Database!');
+            }
+
+            const charSubProf = results[0];
+            const dbProf = await Proficiency.getOne({ id: charSubProf.type });
+
+            return {
+                id: charSubProf.id,
+                char_id: char.id,
+                sub_id: sub.id,
+                name: charSubProf.name,
+                type: dbProf.name,
+                expert: charSubProf.expert,
+            };
+        }
+
+        const results = await query("SELECT * FROM character_subrace_profs WHERE char_id = $1 AND sub_id = $2 AND name = $3", [char.id, sub.id, prof.name])
 
         if (results.length === 0) {
-            throw new NotFoundError('Character Subrace Proficiency not found', 'Could not find that Subrace Proficiency for that Character in the Database!');
+            throw new NotFoundError("Character Subrace Proficiency not found", "Could not find a Subrace Proficiency with that name for that Character in the Database!");
         }
 
         const charSubProf = results[0];
@@ -47,7 +67,7 @@ class CharacterSubraceProf {
         };
     }
 
-    async exists(char, sub, prof) {
+    static async exists(char, sub, prof) {
         if (prof.id) {
             const results = await query('SELECT * FROM character_subrace_profs WHERE char_id = $1 AND sub_id = $2 AND id = $3', [char.id, sub.id, prof.id]);
 
@@ -59,7 +79,7 @@ class CharacterSubraceProf {
         return results.length === 1;
     }
 
-    async add(char, sub, prof) {
+    static async add(char, sub, prof) {
         try {
             const charSubProf = await this.getOne(char, sub, prof);
 
@@ -83,9 +103,26 @@ class CharacterSubraceProf {
         }
     }
 
-    //TODO: Add remove function
+    static async remove(char, sub, prof) {
+        if (!(await this.exists(char, sub, prof))) {
+            throw new NotFoundError('Character Subrace Proficiency not found', 'Could not find that Subrace Proficiency for that Character in the Database!');
+        }
 
-    //TODO: Add update function
+        await query("DELETE FROM character_subrace_profs WHERE char_id = $1 AND sub_id = $2 AND id = $3", [char.id, sub.id, prof.id])
+
+        return "Successfully removed Subrace Proficiency from Character in Database";
+    }
+
+    static async update(char, sub, prof) {
+        if (!(await this.exists(char, sub, prof))) {
+            throw new NotFoundError('Character Subrace Proficiency not found', 'Could not find that Subrace Proficiency for that Character in the Database!');
+        }
+
+        const sql = "UPDATE character_subrace_profs SET name = $1, type = $2, expert = $3 WHERE char_id = $4 AND sub_id = $5 AND id = $6";
+        await query(sql, [prof.name, prof.type, prof.expert, char.id, sub.id, prof.id])
+
+        return "Successfully updated Subrace Proficiency of Character in Database";
+    }
 }
 
 export { CharacterSubraceProf };
