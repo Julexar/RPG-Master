3acbee633d03ac6d0dd7ad3ba7c84056f49ef3d7
Julexar <julexar@gmail.com>
2024-01-22 11:48:12 +0000

    Rewrote Toggle Command

--- commands/slash/Dev/toggle.js
+++ commands/slash/Dev/toggle.js
@@ -1,24 +1,44 @@
 import { ApplicationCommandOptionType, PermissionFlagsBits } from 'discord.js';
-import { glob } from 'glob';
-import { promisify } from 'util';
-const promiseGlob = promisify(glob);
-import fs from 'fs';
-class Command {
-    constructor() {
-        this.name = 'toggle';
-        this.description = 'Toggles a Command';
+import { CommandBuilder } from '../../../custom/builders';
+import { client } from '../../..';
+let cmds;
+
+class Command extends CommandBuilder {
+    constructor(data) {
+        super(data);
+
         this.enabled = true;
-        this.defaultMemberPermissions = [PermissionFlagsBits.Administrator];
-        this.options = [
-            {
-                name: 'command',
-                description: 'Provide a Command',
-                type: ApplicationCommandOptionType.String,
-                required: true,
-            },
-        ];
+        this.choices = true;
     }
 
-    async run(client, interaction) {}
+    /**
+     * @param {import("discord.js").CommandInteraction} interaction
+     */
+    async run(interaction) {
+
+    }
+
+    /**
+     * @param {import("discord.js").Guild} guild
+     */
+    setChoices(guild) {
+        cmds = guild.commands.cache.map(cmd => ({name: cmd.name, value: `${cmd.id}`}));
+    };
 }
-export default new Command();
+
+const command = new Command({
+    name: 'toggle',
+    description: 'Toggles a Command',
+    defaultMemberPermissions: [PermissionFlagsBits.Administrator],
+    options: [
+        {
+            name: 'command',
+            description: 'Provide a Command',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+            choices: cmds
+        }
+    ]
+});
+
+export { command };
