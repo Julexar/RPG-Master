28b149dd8ce18ad5d2ae298d9697e37e4341cce5
Julexar <julexar@gmail.com>
2024-01-22 12:39:27 +0000

    Minor Cleanup

--- commands/prefix/Info/help.js
+++ commands/prefix/Info/help.js
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, PermissionFlagsBits } from 'discord.js';
+import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle } from 'discord.js';
 import { CommandBuilder } from '../../../custom/builders';
 import { client } from '../../..';
 import { ErrorEmbed, ListEmbed } from '../../../custom/embeds';
