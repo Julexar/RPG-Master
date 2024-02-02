aad74bb3074750455e1c4d3b634920113160d21e
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2024-02-02 14:27:45 -0800

    Fixed
    Signed-off-by: WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>

--- custom/embeds/custom-embed.ts
+++ custom/embeds/custom-embed.ts
@@ -4,20 +4,19 @@ class CustomEmbed extends EmbedBuilder {
     constructor(
         title: string,
         description: string,
-        color: ColorResolvable | null = null,
+        color: ColorResolvable | 0x00FFFF = 0x00FFFF,
         fields: APIEmbedField[] | null = null,
-        author: { displayName: string, iconURL: () => string } | undefined
+        author: { displayName: string, iconURL: () => string } | undefined = undefined
     ) {
         super({
-            color: 0x00FFFF,
             description,
             title,
+            timestamp: Date.now(),
             fields: fields || [],
             author: author && { name: author.displayName, iconURL: author.iconURL() },
             footer: { text: 'Made by Julexar' },
         });
 
-        this.setTimestamp();
         this.setColor(color);
     }
 }
