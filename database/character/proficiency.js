52b11708da488cbceece7b1862a6d76550f3b73f
Julexar <julexar@gmail.com>
2024-02-03 14:19:47 +0000

    Finished adding soft-delete

--- database/character/proficiency.js
+++ database/character/proficiency.js
@@ -1,6 +1,6 @@
 import { psql } from '../psql.js';
-import { NotFoundError } from '../../custom/errors';
-import { Proficiency } from '../global';
+import { NotFoundError, DuplicateError, BadRequestError } from '../../custom/errors';
+import { Proficiency } from '..';
 const query = psql.query;
 
 class CharacterProficiency {
@@ -8,20 +8,21 @@ class CharacterProficiency {
         if (!prof.type) {
             const results = await query('SELECT * FROM character_proficiencies WHERE char_id = $1', [char.id]);
 
-            if (results.length === 0) {
-                throw new NotFoundError('No Character Proficiencies found', 'Could not find any Proficiencies for that Character in the Database!');
-            }
+            if (results.length === 0) throw new NotFoundError('No Character Proficiencies found', 'Could not find any Proficiencies for that Character in the Database!');
 
             return Promise.all(
                 results.map(async (charProf) => {
-                    const dbProf = await Proficiency.getOne({ id: charProf.type });
+                    const dbProf = await Proficiency.getOne({ id: charProf.type_id });
+
+                    if (charProf.deleted_at) return;
 
                     return {
                         id: charProf.id,
                         char_id: char.id,
+                        type: dbProf,
                         name: charProf.name,
-                        type: dbProf.name,
                         expert: charProf.expert,
+                        deleted_at: charProf.deleted_at
                     };
                 })
             );
@@ -29,23 +30,21 @@ class CharacterProficiency {
 
         const results = await query('SELECT * FROM character_proficiencies WHERE char_id = $1 AND type = $2', [char.id, prof.type]);
 
-        if (results.length === 0) {
-            throw new NotFoundError(
-                'No Character Proficiencies found',
-                'Could not find any Proficiencies of that type for that Character in the Database!'
-            );
-        }
+        if (results.length === 0) throw new NotFoundError('No Character Proficiencies found', 'Could not find any Proficiencies of that type for that Character in the Database!');
 
         return Promise.all(
             results.map(async (charProf) => {
-                const dbProf = await Proficiency.getOne({ id: charProf.type });
+                const dbProf = await Proficiency.getOne({ id: charProf.type_id });
+
+                if (charProf.deleted_at) return;
 
                 return {
                     id: charProf.id,
                     char_id: char.id,
+                    type: dbProf,
                     name: charProf.name,
-                    type: dbProf.name,
                     expert: charProf.expert,
+                    deleted_at: charProf.deleted_at
                 };
             })
         );
@@ -55,37 +54,39 @@ class CharacterProficiency {
         if (prof.id) {
             const results = await query('SELECT * FROM character_proficiencies WHERE char_id = $1 AND id = $2', [char.id, prof.id]);
 
-            if (results.length === 0) {
-                throw new NotFoundError('Character Proficiency not found', 'Could not find that Proficiency for that Character in the Database!');
-            }
+            if (results.length === 0) throw new NotFoundError('Character Proficiency not found', 'Could not find that Proficiency for that Character in the Database!');
 
             const charProf = results[0];
             const dbProf = await Proficiency.getOne({ id: charProf.type });
 
+            if (charProf.deleted_at) throw new BadRequestError('Character Proficiency deleted', 'The Proficiency of that Character that you are trying to view has been deleted!');
+
             return {
                 id: charProf.id,
                 char_id: char.id,
+                type: dbProf,
                 name: charProf.name,
-                type: dbProf.name,
                 expert: charProf.expert,
+                deleted_at: charProf.deleted_at
             };
         }
 
         const results = await query('SELECT * FROM character_proficiencies WHERE char_id = $1 AND name = $2', [char.id, prof.name]);
 
-        if (results.length === 0) {
-            throw new NotFoundError('Character Proficiency not found', 'Could not find a Character Proficiency with that name in the Database!');
-        }
+        if (results.length === 0) throw new NotFoundError('Character Proficiency not found', 'Could not find a Character Proficiency with that name in the Database!');
 
         const charProf = results[0];
         const dbProf = await Proficiency.getOne({ id: charProf.type });
 
+        if (charProf.deleted_at) throw new BadRequestError('Character Proficiency deleted', 'The Proficiency of that Character that you are trying to view has been deleted!');
+
         return {
             id: charProf.id,
             char_id: char.id,
+            type: dbProf,
             name: charProf.name,
-            type: dbProf.name,
             expert: charProf.expert,
+            deleted_at: charProf.deleted_at
         };
     }
 
@@ -101,22 +102,30 @@ class CharacterProficiency {
         return results.length === 1;
     }
 
+    static async isDeleted(char, prof) {
+        if (prof.id) {
+            const results = await query('SELECT * FROM character_proficiencies WHERE char_id = $1 AND id = $2', [char.id, prof.id]);
+
+            return !!results[0].deleted_at;
+        }
+
+        const results = await query('SELECT * FROM character_proficiencies WHERE char_id = $1 AND name = $2', [char.id, prof.name]);
+
+        return !!results[0].deleted_at;
+    }
+
     static async add(char, prof) {
         try {
             const charProf = await this.getOne(char, prof);
 
-            if (prof.expert === charProf.expert) {
-                throw new DuplicateError('Duplicate Character Proficiency', 'That Character already has that Proficiency!');
-            }
+            if (prof.expert === charProf.expert) throw new DuplicateError('Duplicate Character Proficiency', 'That Character already has that Proficiency!');
 
             const sql = 'UPDATE character_proficiencies SET expert = $1 WHERE char_id = $2 AND id = $3';
             await query(sql, [prof.expert, char.id, charProf.id]);
 
             return 'Successfully updated Character Proficiency in Database';
         } catch (err) {
-            if (!(err instanceof NotFoundError)) {
-                throw err;
-            }
+            if (!(err instanceof NotFoundError)) throw err;
 
             const sql = 'INSERT INTO character_proficiencies (char_id, name, type, expert) VALUES($1, $2, $3, $4)';
             await query(sql, [char.id, prof.name, prof.type, prof.expert]);
@@ -126,13 +135,42 @@ class CharacterProficiency {
     }
 
     static async remove(char, prof) {
-        if (!(await this.exists(char, prof))) {
-            throw new NotFoundError('Character Proficiency not found', 'Could not find that Proficiency for that Character in the Database!');
-        }
+        if (!(await this.exists(char, prof))) throw new NotFoundError('Character Proficiency not found', 'Could not find that Proficiency for that Character in the Database!');
+
+        if (await this.isDeleted(char, prof)) throw new BadRequestError('Character Proficiency deleted', 'The Proficiency of that Character that you are trying to remove has already been deleted!');
+
+        const sql = 'UPDATE character_proficiencies SET deleted_at = $1 WHERE char_id = $2 AND id = $3';
+        await query(sql, [Date.now(), char.id, prof.id]);
+
+        return 'Successfully deleted Character Proficiency';
+    }
+
+    static async remove_final(char, prof) {
+        if (!(await this.exists(char, prof))) throw new NotFoundError('Character Proficiency not found', 'Could not find that Proficiency for that Character in the Database!');
 
         await query('DELETE FROM character_proficiencies WHERE char_id = $1 AND id = $2', [char.id, prof.id]);
 
         return 'Successfully removed Character Proficiency from Database';
+    }
+
+    static async update(char, prof) {
+        if (!(await this.exists(char, prof))) throw new NotFoundError('Character Proficiency not found', 'Could not find that Proficiency for that Character in the Database!');
+
+        if (await this.isDeleted(char, prof)) throw new BadRequestError('Character Proficiency deleted', 'The Proficiency of that Character that you are trying to update has been deleted!');
+
+        const sql = 'UPDATE character_proficiencies SET name = $1, type = $2, expert = $3 WHERE char_id = $4 AND id = $5';
+        await query(sql, [prof.name, prof.type, prof.expert, char.id, prof.id]);
+
+        return 'Successfully updated Character Proficiency in Database';
+    }
+
+    static async restore(char, prof) {
+        if (!(await this.exists(char, prof))) throw new NotFoundError('Character Proficiency not found', 'Could not find that Proficiency for that Character in the Database!');
+
+        const sql = 'UPDATE character_proficiencies SET deleted_at = NULL WHERE char_id = $1 AND id = $2';
+        await query(sql, [char.id, prof.id]);
+
+        return 'Successfully restored Character Proficiency in Database';
     }
 }
 
--- database/character/race/feat.js
+++ database/character/race/feat.js
@@ -1,5 +1,5 @@
 import { psql } from '../../psql.js';
-import { NotFoundError, DuplicateError } from '../../../custom/errors';
+import { NotFoundError, DuplicateError, BadRequestError } from '../../../custom/errors';
 import { ServerFeats } from '../../server/feat.js';
 const query = psql.query;
 
@@ -7,21 +7,21 @@ class CharacterRaceFeat {
     static async getAll(server, char, race) {
         const results = await query('SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2', [char.id, race.id]);
 
-        if (results.length === 0) {
-            throw new NotFoundError('No Character Race Feats found', 'Could not find any racial Feats for that Character in the Database!');
-        }
+        if (results.length === 0) throw new NotFoundError('No Character Race Feats found', 'Could not find any racial Feats for that Character in the Database!');
 
         return Promise.all(
             results.map(async (charRaceFeat) => {
                 const dbFeat = await ServerFeats.getOne(server, { id: charRaceFeat.feat_id });
 
+                if (charRaceFeat.deleted_at) return;
+
                 return {
                     id: charRaceFeat.id,
                     char_id: char.id,
                     race_id: race.id,
-                    feat_id: dbFeat.id,
-                    name: dbFeat.name,
-                    description: dbFeat.description,
+                    feat: dbFeat,
+                    overwrites: charRaceFeat.overwrites,
+                    deleted_at: charRaceFeat.deleted_at
                 };
             })
         );
@@ -29,80 +29,78 @@ class CharacterRaceFeat {
 
     static async getOne(server, char, race, feat) {
         if (feat.id) {
-            const results = await query('SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND id = $3', [
-                char.id,
-                race.id,
-                feat.id,
-            ]);
+            const sql = 'SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND id = $3';
+            const results = await query(sql, [char.id, race.id, feat.id]);
 
-            if (results.length === 0) {
-                throw new NotFoundError('Character Race Feat not found', 'Could not find that racial Feat for that Character in the Database!');
-            }
+            if (results.length === 0) throw new NotFoundError('Character Race Feat not found', 'Could not find that racial Feat for that Character in the Database!');
 
             const charRaceFeat = results[0];
             const dbFeat = await ServerFeats.getOne(server, { id: charRaceFeat.feat_id });
+
+            if (charRaceFeat.deleted_at) throw new BadRequestError('Character Race Feat deleted', 'The racial Feat you are trying to view for that Character has been deleted!');
 
             return {
                 id: charRaceFeat.id,
                 char_id: char.id,
                 race_id: race.id,
-                feat_id: dbFeat.id,
-                name: dbFeat.name,
-                description: dbFeat.description,
+                feat: dbFeat,
+                overwrites: charRaceFeat.overwrites,
+                deleted_at: charRaceFeat.deleted_at
             };
         }
 
         const dbFeat = await ServerFeats.getOne(server, { name: feat.name });
-        const results = await query('SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND feat_id = $3', [
-            char.id,
-            race.id,
-            dbFeat.id,
-        ]);
+        const sql = 'SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND feat_id = $3';
+        const results = await query(sql, [char.id, race.id, dbFeat.id]);
 
-        if (results.length === 0) {
-            throw new NotFoundError(
-                'Character Race Feat not found',
-                'Could not find a racial Feat with that name for that Character in the Database!'
-            );
-        }
+        if (results.length === 0) throw new NotFoundError('Character Race Feat not found', 'Could not find a racial Feat with that name for that Character in the Database!');
 
         const charRaceFeat = results[0];
+
+        if (charRaceFeat.deleted_at) throw new BadRequestError('Character Race Feat deleted', 'The racial Feat you are trying to view for that Character has been deleted!');
 
         return {
             id: charRaceFeat.id,
             char_id: char.id,
             race_id: race.id,
-            feat_id: dbFeat.id,
-            name: dbFeat.name,
-            description: dbFeat.description,
+            feat: dbFeat,
+            overwrites: charRaceFeat.overwrites,
+            deleted_at: charRaceFeat.deleted_at
         };
     }
 
     static async exists(server, char, race, feat) {
         if (feat.id) {
-            const results = await query('SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND id = $3', [
-                char.id,
-                race.id,
-                feat.id,
-            ]);
+            const sql = 'SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND id = $3';
+            const results = await query(sql, [char.id, race.id, feat.id]);
 
             return results.length === 1;
         }
 
         const dbFeat = await ServerFeats.getOne(server, { name: feat.name });
-        const results = await query('SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND feat_id = $3', [
-            char.id,
-            race.id,
-            dbFeat.id,
-        ]);
+        const sql = 'SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND feat_id = $3';
+        const results = await query(sql, [char.id, race.id, dbFeat.id]);
 
         return results.length === 1;
     }
 
-    static async add(server, char, race, feat) {
-        if (await this.exists(server, char, race, feat)) {
-            throw new DuplicateError('Duplicate Character Race Feat', 'That Character already has that racial Feat!');
+    static async isDeleted(server, char, race, feat) {
+        if (feat.id) {
+            const sql = 'SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND id = $3';
+            const results = await query(sql, [char.id, race.id, feat.id])
+
+            return !!results[0].deleted_at;
         }
+
+        const dbFeat = await ServerFeats.getOne(server, { name: feat.name });
+        const sql = 'SELECT * FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND feat_id = $3';
+        const results = await query(sql, [char.id, race.id, dbFeat.id]);
+
+        return !!results[0].deleted_at;
+    }
+
+    static async add(server, char, race, feat) {
+        if (await this.exists(server, char, race, feat)) throw new DuplicateError('Duplicate Character Race Feat', 'That Character already has that racial Feat!');
 
         const sql = 'INSERT INTO character_race_feats (char_id, race_id, feat_id) VALUES($1, $2, $3)';
         await query(sql, [char.id, race.id, feat.feat_id]);
@@ -111,9 +109,18 @@ class CharacterRaceFeat {
     }
 
     static async remove(server, char, race, feat) {
-        if (!(await this.exists(server, char, race, feat))) {
-            throw new NotFoundError('Character Race Feat not found', 'Could not find that racial Feat for that Character in the Database!');
-        }
+        if (!(await this.exists(server, char, race, feat))) throw new NotFoundError('Character Race Feat not found', 'Could not find that racial Feat for that Character in the Database!');
+
+        if (await this.isDeleted(server, char, race, feat)) throw new BadRequestError('Character Race Feat deleted', 'The racial Feat you are trying to remove has already been deleted!');
+
+        const sql = 'UPDATE character_race_feats SET deleted_at = $1 WHERE char_id = $2 AND race_id = $3 AND id = $4';
+        await query(sql, [Date.now(), char.id, race.id, feat.id])
+
+        return 'Successfully marked racial Feat as deleted for Character in Database';
+    }
+
+    static async remove_final(server, char, race, feat) {
+        if (!(await this.exists(server, char, race, feat))) throw new NotFoundError('Character Race Feat not found', 'Could not find that racial Feat for that Character in the Database!');
 
         await query('DELETE FROM character_race_feats WHERE char_id = $1 AND race_id = $2 AND id = $3', [char.id, race.id, feat.id]);
 
@@ -121,14 +128,25 @@ class CharacterRaceFeat {
     }
 
     static async update(server, char, race, feat) {
-        if (!(await this.exists(server, char, race, feat))) {
-            throw new NotFoundError('Character Race Feat not found', 'Could not find that racial Feat for that Character in the Database!');
-        }
+        if (!(await this.exists(server, char, race, feat))) throw new NotFoundError('Character Race Feat not found', 'Could not find that racial Feat for that Character in the Database!');
 
-        const sql = 'UPDATE character_race_feats SET feat_id = $1 WHERE char_id = $2 AND race_id = $3 AND id = $4';
-        await query(sql, [feat.feat_id, char.id, race.id, feat.id]);
+        if (await this.isDeleted(server, char, race, feat)) throw new BadRequestError('Character Race Feat deleted', 'The racial Feat you are trying to update has been deleted!');
+
+        const sql = 'UPDATE character_race_feats SET overwrites = $1::JSON WHERE char_id = $2 AND race_id = $3 AND id = $4';
+        await query(sql, [feat.overwrites, char.id, race.id, feat.id]);
 
         return 'Successfully updated Character Race Feat in Database';
+    }
+
+    static async restore(server, char, race, feat) {
+        if (!(await this.exists(server, char, race, feat))) throw new NotFoundError('Character Race Feat not found', 'Could not find that racial Feat for that Character in the Database!');
+
+        if (!(await this.isDeleted(server, char, race, feat))) throw new BadRequestError('Character Race Feat not deleted', 'The racial Feat you are trying to restore has not been deleted!');
+
+        const sql = 'UPDATE character_race_feats SET deleted_at = NULL WHERE char_id = $1 AND race_id = $2 AND id = $3';
+        await query(sql, [char.id, race.id, feat.id])
+
+        return 'Successfully restored racial Feat for Character in Database';
     }
 }
 
