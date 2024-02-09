db05e47f568ee398e52825a4ae5f86b9d6fd3727
Julexar <julexar@gmail.com>
2024-02-09 11:55:06 +0000

    Rewrote Server Note in TS

--- database/server/note.ts
+++ database/server/note.ts
@@ -1,13 +1,30 @@
-import { psql } from '../psql.js';
-import { NotFoundError, DuplicateError, BadRequestError } from '../../custom/errors/index.js';
-import { User } from '../user';
+import { Guild, User } from 'discord.js';
+import { psql } from '../psql.ts';
+import { NotFoundError, DuplicateError, BadRequestError } from '../../custom/errors';
+import { User as DBUser } from '..';
 const query = psql.query;
 
-class ServerNote {
-    static async getAll(server, user) {
-        if (!(await User.exists(user))) throw new NotFoundError('User not found', 'Could not find that User in the Database!');
+interface DBServerNote {
+    id: bigint;
+    server_id: bigint;
+    user_id: bigint;
+    title: string;
+    content: string;
+    private: boolean;
+    deleted_at: Date | null;
+}
 
-        const results = await query('SELECT * FROM server_notes WHERE server_id = $1 AND user_id = $2', [server.id, user.id]);
+interface AddNote {
+    title: string;
+    content: string;
+    private: boolean;
+}
+
+class ServerNote {
+    static async getAll(server: Guild, user: User) {
+        if (!(await DBUser.exists(user))) throw new NotFoundError('User not found', 'Could not find that User in the Database!');
+
+        const results = await query('SELECT * FROM server_notes WHERE server_id = $1 AND user_id = $2', [server.id, user.id]) as DBServerNote[];
 
         if (results.length === 0) throw new NotFoundError('No Server Notes found', 'Could not find any Server Notes for that User in the Database!');
 
@@ -18,13 +35,12 @@ class ServerNote {
         });
     }
 
-    static async getOne(server, user, note) {
+    static async getOne(server: Guild, user: User, note: { id?: bigint; title?: string }) {
         if (await this.isDeleted(server, user, note)) throw new BadRequestError('Server Note deleted', 'The Server Note you are trying to view has been deleted!')
 
         if (note.id) {
             const sql = 'SELECT * FROM server_notes WHERE server_id = $1 AND user_id = $2 AND id = $3';
-
-            const results = await query(sql, [server.id, user.id, note.id]);
+            const results = await query(sql, [server.id, user.id, note.id]) as DBServerNote[];
 
             if (results.length === 0) throw new NotFoundError('Server Note not found', 'Could not find that Server Note for that User in the Database!');
 
@@ -32,51 +48,42 @@ class ServerNote {
         }
 
         const sql = 'SELECT * FROM server_notes WHERE server_id = $1 AND user_id = $2 AND title = $3';
-
-        const results = await query(sql, [server.id, user.id, note.title]);
+        const results = await query(sql, [server.id, user.id, note.title]) as DBServerNote[];
 
         if (results.length === 0) throw new NotFoundError('Server Note not found', 'Could not find a Server Note with that title for that User in the Database!');
 
         return results[0];
     }
 
-    static async exists(server, user, note) {
+    static async exists(server: Guild, user: User, note: { id?: bigint; title?: string }) {
         if (note.id) {
             const sql = 'SELECT * FROM server_notes WHERE server_id = $1 AND user_id = $2 AND id = $3'
-
-            const results = await query(sql, [server.id, user.id, note.id]);
+            const results = await query(sql, [server.id, user.id, note.id]) as DBServerNote[];
 
             return results.length === 1;
         }
 
         const sql = 'SELECT * FROM server_notes WHERE server_id = $1 AND user_id = $2 AND title = $3';
-
-        const results = await query(sql, [server.id, user.id, note.title]);
+        const results = await query(sql, [server.id, user.id, note.title]) as DBServerNote[];
 
         return results.length === 1;
     }
 
-    static async isDeleted(server, user, note) {
+    static async isDeleted(server: Guild, user: User, note: { id?: bigint; title?: string }) {
         if (note.id) {
             const sql = 'SELECT * FROM server_notes WHERE server_id = $1 AND user_id = $2 AND id = $3';
-
-            const results = await query(sql, [server.id, user.id, note.id]);
-
-            if (results.length === 0) throw new NotFoundError('Server Note not found', 'Could not find that Server Note for that User in the Database!');
+            const results = await query(sql, [server.id, user.id, note.id]) as DBServerNote[];
 
             return !!results[0].deleted_at;
         }
 
         const sql = 'SELECT * FROM server_notes WHERE server_id = $1 AND user_id = $2 AND title = $3';
-
-        const results = await query(sql, [server.id, user.id, note.title]);
-
-        if (results.length === 0) throw new NotFoundError('Server Note not found', 'Could not find a Server Note with that title for that User in the Database!');
+        const results = await query(sql, [server.id, user.id, note.title]) as DBServerNote[];
 
         return !!results[0].deleted_at;
     }
 
-    static async add(server, user, note) {
+    static async add(server: Guild, user: User, note: AddNote) {
         try {
             const serverNote = await this.getOne(server, user, note);
 
@@ -96,7 +103,7 @@ class ServerNote {
         }
     }
 
-    static async remove_final(server, user, note) {
+    static async remove_final(server: Guild, user: User, note: { id: bigint }) {
         if (!(await this.exists(server, user, note))) throw new NotFoundError('Server Note not found', 'Could not find that Server Note for that User in the Database!');
 
         await query('DELETE FROM server_notes WHERE server_id = $1 AND user_id = $2 AND id = $3', [server.id, user.id, note.id]);
@@ -104,8 +111,9 @@ class ServerNote {
         return 'Successfully removed Server Note from Database';
     }
 
-    static async remove(server, user, note) {
+    static async remove(server: Guild, user: User, note: { id: bigint }) {
         if (!(await this.exists(server, user, note))) throw new NotFoundError('Server Note not found', 'Could not find that Server Note for that User in the Database!');
+        
         if (await this.isDeleted(server, user, note)) throw new BadRequestError('Server Note deleted', 'The Server Note you are trying to delete has already been deleted!');
 
         await query('UPDATE server_notes SET deleted_at = $1 WHERE server_id = $2 AND user_id = $3 AND id = $4', [Date.now(), server.id, user.id, note.id]);
@@ -113,7 +121,7 @@ class ServerNote {
         return 'Successfully removed Server Note from Database';
     }
 
-    static async update(server, user, note) {
+    static async update(server: Guild, user: User, note: DBServerNote) {
         if (!(await this.exists(server, user, note))) throw new NotFoundError('Server Note not found', 'Could not find that Server Note for that User in the Database!');
 
         if (await this.isDeleted(server, user, note)) throw new BadRequestError('Server Note deleted', 'The Server Note you are trying to update has been deleted!');
@@ -122,6 +130,16 @@ class ServerNote {
         await query(sql, [note.title, note.content, note.private, server.id, user.id, note.id]);
 
         return 'Successfully updated Server Note in Database';
+    }
+
+    static async restore(server: Guild, user: User, note: { id: bigint }) {
+        if (!(await this.exists(server, user, note))) throw new NotFoundError('Server Note not found', 'Could not find that Server Note for that User in the Database!');
+
+        if (!(await this.isDeleted(server, user, note))) throw new BadRequestError('Server Note not deleted', 'The Server Note you are trying to restore has not been deleted!');
+
+        await query('UPDATE server_notes SET deleted_at = NULL WHERE server_id = $1 AND user_id = $2 AND id = $3', [server.id, user.id, note.id]);
+
+        return 'Successfully restored Server Note in Database';
     }
 }
 
