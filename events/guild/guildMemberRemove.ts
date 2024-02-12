09e406560b4763c215973fdc4a14cf77ae8fd937
Julexar <julexar@gmail.com>
2024-02-12 08:22:24 +0000

    Rewrote GuildMember Events

--- events/guild/guildMemberRemove.ts
+++ events/guild/guildMemberRemove.ts
@@ -1,15 +1,13 @@
+import { GuildMember } from 'discord.js';
 import { client } from '../..';
 
 class Event {
+    name: string;
     constructor() {
         this.name = 'guildMemberRemove';
     }
 
-    /**
-     *
-     * @param {import("discord.js").GuildMember} member
-     */
-    async run(member) {
+    async run(member: GuildMember) {
         const server = member.guild;
 
         await client.database.Server.members.remove(server, member)
