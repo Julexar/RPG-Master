dace5f4a9acf8e0fbff453f08544d2f68c299502
Julexar <julexar@gmail.com>
2024-03-08 07:43:40 +0000

    Updated global Stats

--- database/global/stats.ts
+++ database/global/stats.ts
@@ -1,21 +1,9 @@
-import { psql } from "../psql";
+import { prisma as db } from "../prisma";
 import { NotFoundError, DuplicateError } from "../../custom/errors";
-const { query } = psql;
-
-interface DBStats {
-    id: number;
-    name: string;
-    key: string;
-}
-
-interface AddStats {
-    name: string;
-    key: string;
-}
 
 export class Stats {
     static async getAll() {
-        const results = await query('SELECT * FROM stats') as DBStats[];
+        const results = await db.stats.findMany();
 
         if (results.length === 0) throw new NotFoundError('No Stats found', 'Could not find any Stats in the Database!');
 
@@ -24,51 +12,50 @@ export class Stats {
 
     static async getOne(stats: { id?: number, name?: string, key?: string }) {
         if (stats.id) {
-            const results = await query('SELECT * FROM stats WHERE id = $1', [stats.id]) as DBStats[];
+            const result = await db.stats.findUnique({ where: { id: stats.id } });
 
-            if (results.length === 0) throw new NotFoundError('Stats not found', 'Could not find that Stats in the Database!');
+            if (!result) throw new NotFoundError('Stats not found', 'Could not find that Stats in the Database!');
 
-            return results[0];
+            return result;
         }
 
         if (stats.key) {
-            const results = await query('SELECT * FROM stats WHERE key = $1', [stats.key]) as DBStats[];
+            const result = await db.stats.findUnique({ where: { key: stats.key } });
 
-            if (results.length === 0) throw new NotFoundError('Stats not found', 'Could not find a Stats with that Key in the Database!');
+            if (!result) throw new NotFoundError('Stats not found', 'Could not find a Stats with that Key in the Database!');
 
-            return results[0];
+            return result
         }
 
-        const results = await query('SELECT * FROM stats WHERE name = $1', [stats.name]) as DBStats[];
+        const result = await db.stats.findFirst({ where: { name: stats.name } });
 
-        if (results.length === 0) throw new NotFoundError('Stats not found', 'Could not find a Stats with that Name in the Database!');
+        if (!result) throw new NotFoundError('Stats not found', 'Could not find a Stats with that Name in the Database!');
 
-        return results[0];
+        return result;
     }
 
     static async exists(stats: { id?: number, name?: string, key?: string }) {
         if (stats.id) {
-            const results = await query('SELECT * FROM stats WHERE id = $1', [stats.id]) as DBStats[];
+            const result = await db.stats.findUnique({ where: { id: stats.id } });
 
-            return results.length === 1;
+            return !!result;
         }
 
         if (stats.key) {
-            const results = await query('SELECT * FROM stats WHERE key = $1', [stats.key]) as DBStats[];
+            const result = await db.stats.findUnique({ where: { key: stats.key } });
 
-            return results.length === 1;
+            return !!result;
         }
 
-        const results = await query('SELECT * FROM stats WHERE name = $1', [stats.name]) as DBStats[];
+        const result = await db.stats.findFirst({ where: { name: stats.name } });
 
-        return results.length === 1;
+        return !!result;
     }
 
-    static async add(stats: AddStats) {
+    static async add(stats: { name: string, key: string }) {
         if (await this.exists(stats)) throw new DuplicateError('Stats already exists', 'A Stats with that Name or Key already exists in the Database!');
 
-        const sql = 'INSERT INTO stats (name, key) VALUES ($1, $2)';
-        await query(sql, [stats.name, stats.key]);
+        await db.stats.create({ data: stats });
 
         return 'Successfully added Stats to the Database';
     }
@@ -76,17 +63,15 @@ export class Stats {
     static async remove(stats: { id: number }) {
         if (!await this.exists({ id: stats.id })) throw new NotFoundError('Stats not found', 'Could not find that Stats in the Database!');
 
-        const sql = 'DELETE FROM stats WHERE id = $1';
-        await query(sql, [stats.id]);
+        await db.stats.delete({ where: { id: stats.id } });
 
         return 'Successfully removed Stats from the Database';
     }
 
-    static async update(stats: DBStats) {
+    static async update(stats: { id: number, name: string, key: string }) {
         if (!await this.exists({ id: stats.id })) throw new NotFoundError('Stats not found', 'Could not find that Stats in the Database!');
 
-        const sql = 'UPDATE stats SET name = $1, key = $2 WHERE id = $3';
-        await query(sql, [stats.name, stats.key, stats.id]);
+        await db.stats.update({ data: { name: stats.name, key: stats.key }, where: { id: stats.id } });
 
         return 'Successfully updated Stats in Database';
     }
